d805043b8cbe01195448e0dfac7256c391327f3d
Nishanshah54 <Nessanshahi54@gmail.com>
2024-06-23 09:11:55 +0545

    add function

--- Basic Level (1-30)/Hello_World_Program.java
+++ Basic Level (1-30)/Hello_World_Program.java
@@ -1,7 +1,15 @@
 class Hello_World_Program
 {
+  public  static void Dispay_message()
+  {
+    System.out.println("Hello message from Dispay_message function");
+  }
+    
     public static void main(String[] args) 
     {
-      System.out.println("Hello world!!!!!!!!!!");   
+      System.out.println("Hello world!!!!!!!!!!"); 
+      Dispay_message();
+      {
     }
+  }
 }
